a732c7e12916378dedf23fa4441656bdc9acc685
MatiasFaya <fayamatias@gmail.com>
2026-07-13 10:54:07 -0300

    tutorial

--- majusa/scripts/menu_principal.gd
+++ majusa/scripts/menu_principal.gd
@@ -4,7 +4,7 @@ func _on_boton_jugar_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/nivel_1.tscn")
 
 func _on_boton_opciones_pressed() -> void:
-	pass
+	get_tree().change_scene_to_file("res://scenes/tutorial.tscn")
 
 func _on_boton_salir_pressed() -> void:
 	get_tree().quit()
