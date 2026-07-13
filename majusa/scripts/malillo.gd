b493fa1a2a1ec4e652442cc72e35c709cf7f0106
SantiOrtiz <santiagogortiz@gmail.com>
2026-07-12 21:49:53 -0300

    enemigo rebotin

--- majusa/scripts/malillo.gd
+++ majusa/scripts/malillo.gd
@@ -1,25 +1,28 @@
 extends CharacterBody2D
 
+var gravedad = ProjectSettings.get_setting("physics/2d/default_gravity")
+var daño_enemigo: int = 1
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
 
+@onready var zona_golpe = $Area2D
+
+func _ready() -> void:
+	zona_golpe.body_entered.connect(_on_zona_golpe_body_entered)
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity.y += gravedad * delta
 	move_and_slide()
+
+
+func _on_zona_golpe_body_entered(body: Node2D) -> void:
+	if body is JugadorBase:
+		if body.velocity.y > 0 and body.global_position.y < global_position.y:
+			if body.has_method("rebotar_en_enemigo"):
+				body.rebotar_en_enemigo()
+			queue_free() 
+		else:
+			if body.has_method("recibir_daño"):
+				body.recibir_daño(daño_enemigo)
+				print("ñam")
+			queue_free()
